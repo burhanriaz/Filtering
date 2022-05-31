d3097b7b8c42fe733e5f3dce26d64f0265d3c166
Burhan <Burhan@DESKTOP-9NRFKA1>
2022-05-31 09:23:48 +0500

    Add project files.

---
+++ Custom Filter/ResponseAttribute.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace Filtering.Custom_Filter
+{
+    public class ResponseAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var controllerName = filterContext.RouteData.Values["controller"].ToString();
+            filterContext.HttpContext.Response.Headers.Add("ControlerName", controllerName);
+
+            var actionName = filterContext.RouteData.Values["action"].ToString();
+            filterContext.HttpContext.Response.Headers.Add("ActionName", actionName);
+
+            var scheme = filterContext.HttpContext.Request.Scheme;
+            filterContext.HttpContext.Response.Headers.Add("schemeName", scheme);
+
+            //var port = filterContext.HttpContext.Connection.LocalPort.ToString();
+            var port = filterContext.HttpContext.Request.Host.Port.ToString();
+
+            filterContext.HttpContext.Response.Headers.Add("portName", port);
+
+            var host = filterContext.HttpContext.Request.Host.ToString();
+            filterContext.HttpContext.Response.Headers.Add("HostName", host);
+
+
+            string DateTime = System.DateTime.Now.ToString();
+            filterContext.HttpContext.Response.Headers.Add("DateTime", DateTime);
+
+            var watch = new Stopwatch();
+
+            //filterContext.HttpContext.Response.OnStarting.Subscribe(watch.Elapsed);
+            //{ watch.Stop();
+            //    filterContext.HttpContext.Response.Headers.Add("ExcuationTime",
+            //                    new[] { (watch.ElapsedMilliseconds).ToString() });
+            //}
+            
+
+                base.OnActionExecuted(filterContext);
+        }
+
+        public class ProcessingTimeMiddleware
+        {
+            private readonly RequestDelegate _next;
+
+            public ProcessingTimeMiddleware(RequestDelegate next)
+            {
+                _next = next;
+            }
+
+            public async Task Invoke(HttpContext filterContext)
+            {
+                var watch = new Stopwatch();
+
+                filterContext.Response.OnStarting(() =>
+                {
+                    
+                    watch.Stop();
+
+                    filterContext.Response.Headers.Add("ExcuationTime",
+                                    new[] { (watch.ElapsedMilliseconds).ToString() });
+
+                    return Task.CompletedTask;
+                });
+
+                watch.Start();
+
+                await _next(filterContext);
+            }
+
+        }
+
+
+
+    }
+      
+
+
+    }
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Filtering.Custom_Filter;
+using static Filtering.Controllers.WeatherForecastController;
+using static Filtering.Custom_Filter.ResponseAttribute;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<ResponseAttribute>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseMiddleware<ProcessingTimeMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
